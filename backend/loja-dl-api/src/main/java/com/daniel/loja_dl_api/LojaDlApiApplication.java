b4095062099920268dda487a7cb0faf48a1f2d51
dlzada <danielluiz1607@gmail.com>
2026-06-11 08:23:45 -0300

    initial commit with configurations of db

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/LojaDlApiApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.loja_dl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDlApiApplication.class, args);
+	}
+
+}
