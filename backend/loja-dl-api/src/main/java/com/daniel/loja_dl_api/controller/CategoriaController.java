d508408f76c6e12182ad9dc75ca12076a9f7ce74
dlzada <danielluiz1607@gmail.com>
2026-06-12 08:28:08 -0300

    feat: creation controller of Categoria

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/controller/CategoriaController.java
@@ -0,0 +1,31 @@
+package com.daniel.loja_dl_api.controller;
+
+import com.daniel.loja_dl_api.domain.model.dto.CategoriaRequestDTO;
+import com.daniel.loja_dl_api.domain.model.dto.CategoriaResponseDTO;
+import com.daniel.loja_dl_api.service.CategoriaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+    private final CategoriaService categoriaService;
+
+    @PostMapping
+    public ResponseEntity<CategoriaResponseDTO> cadastrar(@RequestBody @Valid CategoriaRequestDTO dto){
+        CategoriaResponseDTO responseDTO = categoriaService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaResponseDTO>> listarTodas(){
+        List <CategoriaResponseDTO> lista = categoriaService.listarTodas();
+        return ResponseEntity.ok(lista);
+    }
+}
