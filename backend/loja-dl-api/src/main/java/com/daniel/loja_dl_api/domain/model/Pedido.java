3657387fd8a95a7d1f7355d3bf57a1bb76d35828
dlzada <danielluiz1607@gmail.com>
2026-06-13 08:16:37 -0300

    feat: creation entity of Pedido

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/model/Pedido.java
@@ -0,0 +1,35 @@
+package com.daniel.loja_dl_api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "tb_pedido")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime dataPedido;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal valorTotal;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemPedido> itens = new ArrayList<>();
+
+}
