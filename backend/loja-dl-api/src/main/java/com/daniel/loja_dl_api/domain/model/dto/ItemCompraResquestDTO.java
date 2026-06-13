28ea42d0d29a663403e5b2007593de0bf36ad6d7
dlzada <danielluiz1607@gmail.com>
2026-06-13 08:33:38 -0300

    feat: creation dto of request of ItemCompra

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/model/dto/ItemCompraResquestDTO.java
@@ -0,0 +1,22 @@
+package com.daniel.loja_dl_api.domain.model.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemCompraResquestDTO {
+
+    @NotNull(message = "O ID do produto é obrigatorio")
+    private Long id;
+
+    @NotNull(message = "É necessário informar uma quantidade!")
+    @Positive(message = "A quantidade precisa ser maior que zero")
+    private Integer quantidade;
+}
