4ea2a574a01a868199fc340d67681a97a4c41b0e
dlzada <danielluiz1607@gmail.com>
2026-06-15 08:44:55 -0300

    feat: creation repository of Usuario

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.daniel.loja_dl_api.domain.repository;
+
+import com.daniel.loja_dl_api.domain.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByEmail(String email);
+}
