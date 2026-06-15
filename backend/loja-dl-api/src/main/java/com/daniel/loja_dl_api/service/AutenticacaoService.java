c939f475d937892ee5a387ca7e793c829c92db66
dlzada <danielluiz1607@gmail.com>
2026-06-15 08:49:20 -0300

    feat: creation AutenticacaoService

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/service/AutenticacaoService.java
@@ -0,0 +1,23 @@
+package com.daniel.loja_dl_api.service;
+
+import com.daniel.loja_dl_api.domain.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacaoService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails usuario = usuarioRepository.findByEmail(username);
+        if (usuario == null){
+            throw new UsernameNotFoundException("Usuario não encontrado com esse e-mail!");
+        }
+        return usuario;
+    }
+}
