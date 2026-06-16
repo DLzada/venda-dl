9ba25d450bd4f6fcd6b6d796659458405cd471b5
dlzada <danielluiz1607@gmail.com>
2026-06-16 07:58:27 -0300

    feat:creation TokenResponseDTO

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/model/dto/TokenResponseDTO.java
@@ -0,0 +1,12 @@
+package com.daniel.loja_dl_api.domain.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class TokenResponseDTO {
+    private String token;
+}
