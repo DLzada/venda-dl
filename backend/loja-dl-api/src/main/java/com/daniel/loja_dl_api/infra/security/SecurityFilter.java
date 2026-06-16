282325a8f13f798aec7ef8a4325921f4beff344f
dlzada <danielluiz1607@gmail.com>
2026-06-16 08:05:34 -0300

    feat: creation class FilterChain in security

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/infra/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.daniel.loja_dl_api.infra.security;
+
+import com.daniel.loja_dl_api.domain.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recuperarToken(request);
+
+        if(tokenJWT!= null){
+            String email = tokenService.validarToken(tokenJWT);
+            UserDetails user = usuarioRepository.findByEmail(email);
+
+            if(user != null){
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+
+}
