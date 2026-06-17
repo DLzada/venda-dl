baaa02464d62d24378f7161ab88d87aea7737e55
dlzada <danielluiz1607@gmail.com>
2026-06-17 08:30:15 -0300

    feat: creation method atualizarStatus in PedidoController

--- backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/controller/PedidoController.java
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/controller/PedidoController.java
@@ -3,6 +3,7 @@ package com.daniel.loja_dl_api.controller;
 import com.daniel.loja_dl_api.domain.model.dto.PedidoDetalhadoResponseDTO;
 import com.daniel.loja_dl_api.domain.model.dto.PedidoRequestDTO;
 import com.daniel.loja_dl_api.domain.model.dto.PedidoResponseDTO;
+import com.daniel.loja_dl_api.domain.model.dto.StatusPedidoRequestDTO;
 import com.daniel.loja_dl_api.service.PedidoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -37,5 +38,12 @@ public class PedidoController {
     public ResponseEntity<PedidoDetalhadoResponseDTO> buscarPedido(@PathVariable Long id){
         PedidoDetalhadoResponseDTO pedido = pedidoService.buscarPorId(id);
         return ResponseEntity.ok(pedido);
+    }
+
+    @PutMapping("/{id}/status")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> atulizarStatus(@PathVariable Long id, @RequestBody @Valid StatusPedidoRequestDTO requestDTO){
+        pedidoService.atualizarPedido(id, requestDTO.getStatus());
+        return ResponseEntity.noContent().build();
     }
 }
