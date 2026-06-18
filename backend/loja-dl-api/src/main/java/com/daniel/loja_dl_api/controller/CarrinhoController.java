fd0ec96cb9722ee33a143636a1cdc6a150e43138
dlzada <danielluiz1607@gmail.com>
2026-06-18 08:30:59 -0300

    feat: creation CarrinhoController and add methods obterCarrinho and adicionarItem

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/controller/CarrinhoController.java
@@ -0,0 +1,29 @@
+package com.daniel.loja_dl_api.controller;
+
+import com.daniel.loja_dl_api.domain.model.dto.CarrinhoResponseDTO;
+import com.daniel.loja_dl_api.domain.model.dto.ItemCarrinhoRequestDTO;
+import com.daniel.loja_dl_api.service.CarrinhoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/carrinho")
+@PreAuthorize("hasRole('ADMIN') or hasRole('CLIENTE')")
+@RequiredArgsConstructor
+public class CarrinhoController {
+    private final CarrinhoService carrinhoService;
+
+    @GetMapping
+    public ResponseEntity<CarrinhoResponseDTO> obterCarrinho(){
+        return ResponseEntity.ok(carrinhoService.buscarCarrinhoDTO());
+    }
+
+    @PostMapping("/adicionar")
+    public ResponseEntity<CarrinhoResponseDTO> adicionarItem(@RequestBody @Valid ItemCarrinhoRequestDTO requestDTO){
+        return ResponseEntity.ok(carrinhoService.adicionarItem(requestDTO));
+    }
+
+}
