5079fb0d3194699d5b95953c43b1385d65bc4e98
dlzada <danielluiz1607@gmail.com>
2026-06-18 07:36:30 -0300

    feat: add method in PedidoRepository

--- backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/repository/PedidoRepository.java
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/repository/PedidoRepository.java
@@ -1,9 +1,13 @@
 package com.daniel.loja_dl_api.domain.repository;
 
 import com.daniel.loja_dl_api.domain.model.entity.Pedido;
+import com.daniel.loja_dl_api.domain.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByUsuario(Usuario usuario);
 }
