b1d2b2a1d90c316891a0c0276eaff01f61344bf6
dlzada <danielluiz1607@gmail.com>
2026-06-20 08:03:47 -0300

    feat: creation DashboardController with method of get

---
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/controller/DashboardController.java
@@ -0,0 +1,23 @@
+package com.daniel.loja_dl_api.controller;
+
+import com.daniel.loja_dl_api.domain.model.dto.DashboardResumoResponseDTO;
+import com.daniel.loja_dl_api.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @GetMapping("/resumo")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<DashboardResumoResponseDTO> obterResumo(){
+        return ResponseEntity.ok(dashboardService.obterResumoVendas());
+    }
+}
