9141dd717bc06540d7e5e8b5769e9db93a906f32
dlzada <danielluiz1607@gmail.com>
2026-06-20 08:06:46 -0300

    fix: add @entityGraph in PedidoRepository

--- backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/repository/PedidoRepository.java
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/domain/repository/PedidoRepository.java
@@ -3,6 +3,7 @@ package com.daniel.loja_dl_api.domain.repository;
 import com.daniel.loja_dl_api.domain.model.entity.Pedido;
 import com.daniel.loja_dl_api.domain.model.entity.Usuario;
 import com.daniel.loja_dl_api.domain.model.enums.StatusPedido;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,7 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
     List<Pedido> findByUsuario(Usuario usuario);
+
+    @EntityGraph(attributePaths = {"itens"})
     List<Pedido> findByStatus(StatusPedido status);
 }
