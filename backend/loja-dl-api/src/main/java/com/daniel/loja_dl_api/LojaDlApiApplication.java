1abb6554a2a6162a2c2157c21541f658110379ee
dlzada <danielluiz1607@gmail.com>
2026-06-26 07:21:56 -0300

    feat: activate appointments of spring

--- backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/LojaDlApiApplication.java
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/LojaDlApiApplication.java
@@ -2,8 +2,10 @@ package com.daniel.loja_dl_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LojaDlApiApplication {
 
 	public static void main(String[] args) {
