caa125f48f592538d5f9f91086e6249dcfadb89e
dlzada <danielluiz1607@gmail.com>
2026-07-22 09:39:50 -0300

    mend
    integrate angular frontend with spring boot listing api

--- backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/LojaDlApiApplication.java
+++ backend/loja-dl-api/src/main/java/com/daniel/loja_dl_api/LojaDlApiApplication.java
@@ -2,10 +2,12 @@ package com.daniel.loja_dl_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class LojaDlApiApplication {
 
 	public static void main(String[] args) {
